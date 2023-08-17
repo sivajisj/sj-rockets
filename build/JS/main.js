b044bba4de066750589a2d58eee887319e954e2b
Sivaji gadidala <93958018+sivajisj@users.noreply.github.com>
2023-08-17 16:59:54 +0530

    files are added

---
+++ build/JS/main.js
@@ -0,0 +1,28 @@
+// const initApp = ()=>{
+//     const hamburgerbtn = document.getElementById('hamburger-button')
+//     const mobileMenu = document.getElementById('mobile-menu')
+//     const toggleMenu = ()=>{
+//         mobileMenu.classList.toggle('hidden')
+//         mobileMenu.classList.toggle('flex')
+//         hamburgerbtn.classList.toggle('toggle-btn')
+//     }
+//     hamburgerbtn.addEventListener("click",toggleMenu)
+//     mobileMenu.addEventListener('click',toggleMenu)
+// }
+
+// document.addEventListener('DOMContentLoaded',initApp)
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hamburgerBtn.classList.toggle('toggle-btn')
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
